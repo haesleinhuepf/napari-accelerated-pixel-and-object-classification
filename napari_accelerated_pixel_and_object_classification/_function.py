fb53fc1c6aabef529873575483ca854e0f09294b
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-10-01 21:52:06 +0200

    added support for multi-channel segmentation

--- napari_accelerated_pixel_and_object_classification/_function.py
+++ napari_accelerated_pixel_and_object_classification/_function.py
@@ -1,11 +1,9 @@
 from apoc import PredefinedFeatureSet, PixelClassifier, ObjectSegmenter, ObjectClassifier
-from typing import TYPE_CHECKING
 
 import numpy as np
 from napari_plugin_engine import napari_hook_implementation
 
-if TYPE_CHECKING:
-    import napari
+import napari
 
 
 @napari_hook_implementation
@@ -13,7 +11,13 @@ def napari_experimental_provide_function():
     return [
         Train_object_segmentation,
         Apply_object_segmentation,
-        Train_pixel_classifier, Apply_pixel_classification, Connected_component_labeling, Train_object_classifier, Apply_object_classification]
+        Train_object_segmentation_from_visible_image_layers,
+        Apply_object_segmentation_to_visible_image_layers,
+        Train_pixel_classifier,
+        Apply_pixel_classification,
+        Train_pixel_classifier_from_visible_image_layers,
+        Apply_pixel_classification_to_visible_image_layers,
+        Connected_component_labeling, Train_object_classifier, Apply_object_classification]
 
 def Train_pixel_classifier(
         image: "napari.types.ImageData",
@@ -41,6 +45,36 @@ def Apply_pixel_classification(image: "napari.types.ImageData",
     result = clf.predict(image=image)
     return result
 
+def Train_pixel_classifier_from_visible_image_layers(
+        annotation : "napari.types.LabelsData",
+        model_filename : str = "pixel_classifier.cl",
+        featureset : PredefinedFeatureSet = PredefinedFeatureSet.small_quick,
+        custom_features : str = "original gaussian_blur=1 sobel_of_gaussian_blur=1",
+        max_depth : int = 2,
+        num_ensembles : int = 10,
+        napari_viewer : napari.Viewer = None
+) -> "napari.types.LabelsData":
+    image = [layer.data for layer in napari_viewer.layers if (isinstance(layer, napari.layers.Image) and layer.visible)]
+
+    feature_stack = featureset.value
+    if feature_stack == "":
+        feature_stack = custom_features
+
+    clf = PixelClassifier(opencl_filename=model_filename, num_ensembles=num_ensembles, max_depth=max_depth)
+    clf.train(feature_stack, annotation, image)
+
+    result = clf.predict(feature_stack, image)
+    return result
+
+def Apply_pixel_classification_to_visible_image_layers(
+        model_filename : str = "pixel_classifier.cl",
+        napari_viewer : napari.Viewer = None
+) -> "napari.types.LabelsData":
+    image = [layer.data for layer in napari_viewer.layers if (isinstance(layer, napari.layers.Image) and layer.visible)]
+
+    clf = PixelClassifier(opencl_filename=model_filename)
+    result = clf.predict(image=image)
+    return result
 
 def Train_object_segmentation(
         image: "napari.types.ImageData",
@@ -64,6 +98,38 @@ def Train_object_segmentation(
 
 def Apply_object_segmentation(image: "napari.types.ImageData",
                              model_filename : str = "object_segmenter.cl") -> "napari.types.LabelsData":
+
+    clf = ObjectSegmenter(opencl_filename=model_filename)
+    result = clf.predict(image=image)
+    return result
+
+def Train_object_segmentation_from_visible_image_layers(
+        annotation : "napari.types.LabelsData",
+        model_filename : str = "object_segmenter.cl",
+        featureset : PredefinedFeatureSet = PredefinedFeatureSet.small_quick,
+        custom_features : str = "original gaussian_blur=1 sobel_of_gaussian_blur=1",
+        max_depth : int = 2,
+        num_ensembles : int = 10,
+        annotated_object_intensity : int = 2,
+        napari_viewer : napari.Viewer = None
+) -> "napari.types.LabelsData":
+    image = [layer.data for layer in napari_viewer.layers if (isinstance(layer, napari.layers.Image) and layer.visible)]
+
+    feature_stack = featureset.value
+    if feature_stack == "":
+        feature_stack = custom_features
+
+    clf = ObjectSegmenter(opencl_filename=model_filename, num_ensembles=num_ensembles, max_depth=max_depth, positive_class_identifier=annotated_object_intensity)
+    clf.train(feature_stack, annotation, image)
+
+    result = clf.predict(feature_stack, image)
+    return result
+
+def Apply_object_segmentation_to_visible_image_layers(
+        model_filename : str = "object_segmenter.cl",
+        napari_viewer : napari.Viewer = None
+) -> "napari.types.LabelsData":
+    image = [layer.data for layer in napari_viewer.layers if (isinstance(layer, napari.layers.Image) and layer.visible)]
 
     clf = ObjectSegmenter(opencl_filename=model_filename)
     result = clf.predict(image=image)
