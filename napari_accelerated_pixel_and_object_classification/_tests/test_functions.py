beead23fe1ddff4947b94802ed83b46922a087c1
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-12-28 17:01:02 +0100

    run tests without viewer

--- napari_accelerated_pixel_and_object_classification/_tests/test_functions.py
+++ napari_accelerated_pixel_and_object_classification/_tests/test_functions.py
@@ -53,6 +53,41 @@ def test_training(make_napari_viewer):
     Apply_object_segmentation_to_visible_image_layers(napari_viewer=viewer)
     Apply_pixel_classification_to_visible_image_layers(napari_viewer=viewer)
 
+def test_training_without_viewer():
+
+    from napari_accelerated_pixel_and_object_classification._function import Train_object_classifier,\
+        Train_pixel_classifier,\
+        Train_object_segmentation,\
+        Train_object_segmentation_from_visible_image_layers,\
+        Train_pixel_classifier_from_visible_image_layers,\
+        Connected_component_labeling,\
+        Apply_pixel_classification,\
+        Apply_object_segmentation,\
+        Apply_probability_mapper,\
+        Apply_object_segmentation_to_visible_image_layers,\
+        Apply_pixel_classification_to_visible_image_layers,\
+        Apply_object_classification\
+
+    import apoc
+
+    image = np.asarray([
+        [0,0,1,1],
+        [0,0,1,1],
+        [2,2,1,1],
+        [2,2,1,1],
+    ])
+    labels = image.astype(int)
+
+    Train_pixel_classifier(image, labels, featureset=apoc.PredefinedFeatureSet.custom, custom_features="original")
+    Train_pixel_classifier(image, labels)
+    Train_object_segmentation(image, labels)
+    Connected_component_labeling(labels)
+    Connected_component_labeling(labels, fill_gaps_between_labels=False)
+    Apply_object_classification(image, labels)
+    Apply_pixel_classification(image)
+    Apply_object_segmentation(image)
+    Apply_probability_mapper(image)
+
 def test_object_segmentation():
 
     from napari_accelerated_pixel_and_object_classification._function import Train_object_segmentation,\
