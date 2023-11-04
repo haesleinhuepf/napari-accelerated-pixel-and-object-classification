fd02216fec374f5c1c84d74589da2ce0d5d5be13
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-11-04 09:17:09 +0100

    make code generation work

--- napari_accelerated_pixel_and_object_classification/__init__.py
+++ napari_accelerated_pixel_and_object_classification/__init__.py
@@ -1,10 +1,13 @@
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "0.14.0"
+    __version__ = "0.14.1"
 
+__common_alias__ = "napoc"
 
-
+from ._function import apply_object_selection, apply_object_classification, apply_object_segmentation, \
+    apply_pixel_classification, apply_probability_mapper
+from ._object_merger import apply_object_merger
 
 from ._function import napari_experimental_provide_function
 from ._dock_widget import napari_experimental_provide_dock_widget
